cdd55a7bc0cac4aac1e1f9caf44501d579341ee5
wenhai0007 <wen.hai0007@outlook.com>
2021-11-23 12:26:36 +0800

    find top two v2

--- find_top_two.go
+++ find_top_two.go
@@ -28,7 +28,7 @@ func FindTopTwoMaxNumber(array []int) (max, second, indexm, indexs int) {
 		return
 	}
 	max = array[0]
-	second = math.MinInt32 //have to use min, to make sure "else if" logic have to triggered
+	second = math.MinInt32 //have to use min, to make sure second number is compared
 	indexm = 0
 	indexs = 1
 	for i := 1; i < len(array); i++ {
@@ -38,6 +38,52 @@ func FindTopTwoMaxNumber(array []int) (max, second, indexm, indexs int) {
 			indexs = indexm
 			indexm = i
 		} else if array[i] > second {
+			second = array[i]
+			indexs = i
+		}
+	}
+	return
+}
+
+func FindTopTwoMinNumberV2(array []int) (min, second, indexm, indexs int) {
+	if len(array) < 2 {
+		return
+	}
+	min = array[0]
+	second = math.MaxInt32 //have to use max, to make sure second number is compared
+	indexm = 0
+	indexs = 0
+	for i := 1; i < len(array); i++ {
+		if array[i] < min {
+			min = array[i]
+			indexm = i
+		}
+	}
+	for i := 0; i < len(array); i++ {
+		if array[i] != min && array[i] < second {
+			second = array[i]
+			indexs = i
+		}
+	}
+	return
+}
+
+func FindTopTwoMaxNumberV2(array []int) (max, second, indexm, indexs int) {
+	if len(array) < 2 {
+		return
+	}
+	max = array[0]
+	second = math.MinInt32 //have to use min, to make sure "else if" logic have to triggered
+	indexm = 0
+	indexs = 0
+	for i := 1; i < len(array); i++ {
+		if array[i] > max {
+			max = array[i]
+			indexm = i
+		}
+	}
+	for i := 0; i < len(array); i++ {
+		if array[i] != max && array[i] > second {
 			second = array[i]
 			indexs = i
 		}
--- find_top_two_test.go
+++ find_top_two_test.go
@@ -7,6 +7,8 @@ import (
 
 func TestFindTopTwoMaxNumberByHeap(t *testing.T) {
 	arr := []int{222, 2, 34, 6, 2, 45}
-	fmt.Println(FindTopTwoMaxNumber(arr))
+	//fmt.Println(FindTopTwoMaxNumber(arr))
+	arr = []int{0, 1, 2, 3}
+	fmt.Println(FindTopTwoMaxNumberV2(arr))
 
 }
