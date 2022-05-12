d214d3b1c428bb7838968a7590d56cc72ce52d48
wenhai0007 <wen.hai0007@outlook.com>
2022-05-12 21:47:19 +0800

    add slide window to cal max sum

--- play_with_arr/arr.go
+++ play_with_arr/arr.go
@@ -1,5 +1,7 @@
 package play_with_arr
 
+import "fmt"
+
 //有序的数组
 //找到所对应值所在的index
 func FindIndex(arr []int, val int) int {
@@ -42,4 +44,39 @@ func remove_ele(arr []int, val int) int {
 		l--
 	}
 	return slow + 1
+}
+
+//最短的window，相加和>=s
+func add_on(arr []int, s int) int {
+	fast, slow := 0, 0
+	res := len(arr) + 1
+	sum := 0
+	for fast < len(arr) {
+		if arr[fast] >= s {
+			return 1
+		}
+		sum += arr[fast]
+		if sum >= s {
+			if res > fast-slow+1 {
+				res = fast - slow + 1
+			}
+			for slow <= fast {
+				fmt.Println("Sadfd: ", slow, fast)
+				sum -= arr[slow]
+				slow++
+				if sum >= s {
+					if res > fast-slow+1 {
+						res = fast - slow + 1
+					}
+				} else {
+					break
+				}
+			}
+		}
+		fast++
+	}
+	if res == len(arr)+1 {
+		return 0
+	}
+	return res
 }
--- play_with_arr/arr_test.go
+++ play_with_arr/arr_test.go
@@ -10,3 +10,9 @@ func TestARR(t *testing.T) {
 	fmt.Println(FindIndex(arr, 2))
 	fmt.Println(remove_ele(arr, 2))
 }
+
+func TestAadd_on(t *testing.T) {
+	arr := []int{1, 2, 3, 4, 8}
+	s := 9
+	fmt.Println(add_on(arr, s))
+}
